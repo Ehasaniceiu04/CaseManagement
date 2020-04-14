a2b2108bbea263134cfcd858b9c645b7214851d0
Ehasaniceiu04 <ehasan@outlook.com>
2020-04-14 20:00:47 +0600

    first commit

---
+++ case-management-ui/src/app/core/_config/i18n/bn.ts
@@ -0,0 +1,160 @@
+// USA
+export const locale = {
+	lang: 'bn',
+	data: {
+		TRANSLATOR: {
+			SELECT: 'Select your language',
+		},
+		PREFERENCEOPTION:{
+			Yes:"হ্যাঁ",
+			No:"না"
+		 },
+		MENU: {
+			NEW: 'new',
+			ACTIONS: 'Actions',
+			CREATE_POST: 'Create New Post',
+			PAGES: 'Pages',
+			FEATURES: 'Features',
+			APPS: 'Apps',
+			DASHBOARD: 'ড্যাশবোর্ড',
+			CASE:{
+				TITLE:"মামলার তালিকা",
+				CHILD:{
+					ACTIVE:"সক্রিয় কেস",
+					CLOSED:"কেস বন্ধ",
+					REOPENED:"মামলা পুনরায় খোলা"
+				}
+			},
+			OFFICEMANAGEMENT:{
+				TITLE:"অফিস ব্যবস্থাপনা",
+				CHILD:{
+					
+				}
+			},
+			USERMANAGEMENT:{
+				TITLE:"ইউজার ম্যানেজমেন্ট",
+				CHILD:{
+					USERS:"ব্যবহারকারীরা",
+					ROLES:"ভূমিকা",
+				}
+			},
+			FORMS:{
+				TITLE:"ফরম",
+				CHILD:{
+					
+				}
+			},
+			REPORTS:{
+				TITLE:"প্রতিবেদন",
+				CHILD:{
+					
+				}
+			},
+			ACCESSCONTROL:{
+				TITLE:"প্রবেশাধিকার নিয়ন্ত্রণ",
+				CHILD:{
+					
+				}
+			},
+			APPMODULEACCESS:{
+				TITLE:"অ্যাপ্লিকেশন মডিউল অ্যাক্সেস",
+				CHILD:{
+					
+				}
+			},
+			GEOLOCATION:{
+				TITLE:"জিওলোকেশন ম্যানেজমেন্ট",
+				CHILD:{
+					
+				}
+			},
+		},
+		AUTH: {
+			GENERAL: {
+				OR: 'Or',
+				SUBMIT_BUTTON: 'Submit',
+				NO_ACCOUNT: 'Don\'t have an account?',
+				SIGNUP_BUTTON: 'Sign Up',
+				FORGOT_BUTTON: 'Forgot Password',
+				BACK_BUTTON: 'Back',
+				PRIVACY: 'Privacy',
+				LEGAL: 'Legal',
+				CONTACT: 'Contact',
+			},
+			LOGIN: {
+				TITLE: 'Login Account',
+				BUTTON: 'Sign In',
+			},
+			FORGOT: {
+				TITLE: 'Forgotten Password?',
+				DESC: 'Enter your email to reset your password',
+				SUCCESS: 'Your account has been successfully reset.'
+			},
+			REGISTER: {
+				TITLE: 'Sign Up',
+				DESC: 'Enter your details to create your account',
+				SUCCESS: 'Your account has been successfuly registered.'
+			},
+			INPUT: {
+				EMAIL: 'Email',
+				FULLNAME: 'Fullname',
+				PASSWORD: 'Password',
+				CONFIRM_PASSWORD: 'Confirm Password',
+				USERNAME: 'Username'
+			},
+			VALIDATION: {
+				INVALID: '{{name}} is not valid',
+				REQUIRED: '{{name}} is required',
+				MIN_LENGTH: '{{name}} minimum length is {{min}}',
+				AGREEMENT_REQUIRED: 'Accepting terms & conditions are required',
+				NOT_FOUND: 'The requested {{name}} is not found',
+				INVALID_LOGIN: 'The login detail is incorrect',
+				REQUIRED_FIELD: 'Required field',
+				MIN_LENGTH_FIELD: 'Minimum field length:',
+				MAX_LENGTH_FIELD: 'Maximum field length:',
+				INVALID_FIELD: 'Field is not valid',
+			}
+		},
+		ECOMMERCE: {
+			COMMON: {
+				SELECTED_RECORDS_COUNT: 'Selected records count: ',
+				ALL: 'All',
+				SUSPENDED: 'Suspended',
+				ACTIVE: 'Active',
+				FILTER: 'Filter',
+				BY_STATUS: 'by Status',
+				BY_TYPE: 'by Type',
+				BUSINESS: 'Business',
+				INDIVIDUAL: 'Individual',
+				SEARCH: 'Search',
+				IN_ALL_FIELDS: 'in all fields'
+			},
+			ECOMMERCE: 'eCommerce',
+			CUSTOMERS: {
+				CUSTOMERS: 'Customers',
+				CUSTOMERS_LIST: 'Customers list',
+				NEW_CUSTOMER: 'New Customer',
+				DELETE_CUSTOMER_SIMPLE: {
+					TITLE: 'Customer Delete',
+					DESCRIPTION: 'Are you sure to permanently delete this customer?',
+					WAIT_DESCRIPTION: 'Customer is deleting...',
+					MESSAGE: 'Customer has been deleted'
+				},
+				DELETE_CUSTOMER_MULTY: {
+					TITLE: 'Customers Delete',
+					DESCRIPTION: 'Are you sure to permanently delete selected customers?',
+					WAIT_DESCRIPTION: 'Customers are deleting...',
+					MESSAGE: 'Selected customers have been deleted'
+				},
+				UPDATE_STATUS: {
+					TITLE: 'Status has been updated for selected customers',
+					MESSAGE: 'Selected customers status have successfully been updated'
+				},
+				EDIT: {
+					UPDATE_MESSAGE: 'Customer has been updated',
+					ADD_MESSAGE: 'Customer has been created'
+				}
+			}
+		}
+	}
+};
